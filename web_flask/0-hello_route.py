c3aa3330f5f43fa1f00de4402c0057574c008101
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-04-21 16:38:07 -0700

    script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""
+This module starts a Flask web application hosted on 0.0.0.0, port 5000.
+It displays 'Hello HBNB!' at the root directory.
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """ Displays 'Hello HBNB!' as a text response. """
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
